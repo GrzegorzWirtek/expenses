ffd7bc02af58a997a16f5bf0a1c9262e7ec0a4d8
Grzegorz Wirtek <grzegorz.wirtek@wp.pl>
2022-03-24 13:57:35 +0100

    deploying on heroku

--- app.js
+++ app.js
@@ -1,199 +1,146 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-const dbName = 'ExpencesDatabase';
-mongooseURL=`mongodb+srv://admin:Entropia1@cluster0.193v2.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-mongoose.connect(mongooseURL);
+dotenv.config();
+
+mongoose.connect(process.env.MONGOOSE_URL);
 const db = mongoose.connection;
-db.on('error', (err)=> console.error(error));
-db.once('open', (err)=> console.log('Connected to database'));
+db.on('error', (err) => console.error(error));
+db.once('open', (err) => console.log('Connected to database'));
 
 const Schema = mongoose.Schema;
 const newDay = new Schema({
-  id: {type: String, default: 'expences'},
-  money: {type:Number, default: 900},
-  moneyConstant: {type:Number, default: 900},
-  counter: {type: Number, default: 13},
-  day:{ type: Number, default: 25},
-  month:{ type: Number, default: 10},
-  year:{ type: Number, default: 2021},
+	id: { type: String, default: 'expences' },
+	money: { type: Number, default: 900 },
+	moneyConstant: { type: Number, default: 900 },
+	counter: { type: Number, default: 13 },
+	day: { type: Number, default: 25 },
+	month: { type: Number, default: 10 },
+	year: { type: Number, default: 2021 },
 
-  food: {type: Number, default: 0},
-  fuel: {type: Number, default: 0},
-  alcohol: {type: Number, default: 0},
-  medicines: {type: Number, default: 0},
-  bills: {type: Number, default: 0},
-  clothes: {type: Number, default: 0},
-  entertainment: {type: Number, default: 0},
-  chemicals: {type: Number, default: 0},
+	food: { type: Number, default: 0 },
+	fuel: { type: Number, default: 0 },
+	medicines: { type: Number, default: 0 },
+	bills: { type: Number, default: 0 },
+	clothes: { type: Number, default: 0 },
+	entertainment: { type: Number, default: 0 },
+	chemicals: { type: Number, default: 0 },
 
-  foodMonth: {type: Number, default: 0},
-  fuelMonth: {type: Number, default: 0},
-  alcoholMonth: {type: Number, default: 0},
-  medicinesMonth: {type: Number, default: 0},
-  billsMonth: {type: Number, default: 0},
-  clothesMonth: {type: Number, default: 0},
-  entertainmentMonth: {type: Number, default: 0},
-  chemicalsMonth: {type: Number, default: 0}
+	foodMonth: { type: Number, default: 0 },
+	fuelMonth: { type: Number, default: 0 },
+	medicinesMonth: { type: Number, default: 0 },
+	billsMonth: { type: Number, default: 0 },
+	clothesMonth: { type: Number, default: 0 },
+	entertainmentMonth: { type: Number, default: 0 },
+	chemicalsMonth: { type: Number, default: 0 },
 });
-const dayModel = (mongoose.model('day', newDay));
+const dayModel = mongoose.model('day', newDay);
 
 const app = express();
-app.use(express.static(
-  path.join(__dirname, 'public')
-));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.get('/getdata', async (req, res)=>{
-  const actualDate = new Date();
-  const actualDay = actualDate.getDate();
-  const actualMonth = actualDate.getMonth() + 1;
-  const actualYear = actualDate.getFullYear();
+app.get('/getdata', async (req, res) => {
+	const actualDate = new Date();
+	const actualDay = actualDate.getDate();
+	const actualMonth = actualDate.getMonth() + 1;
+	const actualYear = actualDate.getFullYear();
 
-  const data = await dayModel.findOne({id: 'expences'});
-  const dateData = { actualDay, actualMonth, actualYear };
-  const { counter, day, month, money, year } = data;
-  
-  const dateFromDB = new Date(`${month}/${day}/${year}`);
-  const difference = actualDate.getTime() - dateFromDB.getTime();
-  const differenceDays = (Math.ceil(difference / (1000 * 3600 * 24))) - 1;
-  
-  //jeśli jest ten sam dzień, zwróć zaktualizowane dane
-  if(counter === differenceDays) return res.json([data, dateData]);
+	const data = await dayModel.findOne({ id: 'expences' });
+	const dateData = { actualDay, actualMonth, actualYear };
+	const { counter, day, month, money, year } = data;
 
-  //jeśli jest nowy miesiąc rozliczeniowy, aktualizuj
-  if(differenceDays > 27){
-    let temporaryActualMonth = null
-    if(actualDay >= 25){
-      temporaryActualMonth = actualMonth;
-    }else if(actualDay < 25){
-      temporaryActualMonth = actualMonth - 1;
-    };
-    const temporaryInitialDate = new Date(`${temporaryActualMonth}/25/${year}`);
-    const temporaryDifference = actualDate.getTime() - temporaryInitialDate.getTime();
-    const temporaryDifferenceDays = (Math.ceil(temporaryDifference / (1000 * 3600 * 24))) - 1;
-    await dayModel.findOneAndUpdate(
-      {
-        id: 'expences'
-      },{
-        counter: temporaryDifferenceDays,
-        month: temporaryActualMonth,
-        year: actualYear,
-        money: 2850,
-        moneyConstant: 2850,
-        food: 0,
-        fuel: 0,
-        alcohol: 0,
-        medicines: 0,
-        bills: 0,
-        clothes: 0,
-        entertainment: 0,
-        chemicals: 0,
-        foodMonth: 0,
-        fuelMonth: 0,
-        alcoholMonth: 0,
-        medicinesMonth: 0,
-        billsMonth: 0,
-        clothesMonth: 0,
-        entertainmentMonth: 0,
-        chemicalsMonth: 0
-    });
-    const data = await dayModel.findOne({id: 'expences'})
-    res.json([data, dateData]);
-  }else {
-    //jeśli jest nowy dzień, zaktualizuj dane
-    await dayModel.findOneAndUpdate(
-      {
-        id: 'expences'
-      },{
-        counter: differenceDays,
-        moneyConstant: money,
-        food: 0,
-        fuel: 0,
-        alcohol: 0,
-        medicines: 0,
-        bills: 0,
-        clothes: 0,
-        entertainment: 0,
-        chemicals: 0
-      });
-    const data = await dayModel.findOne({id: 'expences'})
-    res.json([data, dateData]);
-  };
+	const dateFromDB = new Date(`${month}/${day}/${year}`);
+	const difference = actualDate.getTime() - dateFromDB.getTime();
+	const differenceDays = Math.ceil(difference / (1000 * 3600 * 24)) - 1;
+
+	//jeśli jest ten sam dzień, zwróć zaktualizowane dane
+	if (counter === differenceDays) return res.json([data, dateData]);
+
+	//jeśli jest nowy miesiąc rozliczeniowy, aktualizuj
+	if (differenceDays > 27) {
+		let temporaryActualMonth = null;
+		if (actualDay >= 25) {
+			temporaryActualMonth = actualMonth;
+		} else if (actualDay < 25) {
+			temporaryActualMonth = actualMonth - 1;
+		}
+		const temporaryInitialDate = new Date(`${temporaryActualMonth}/25/${year}`);
+		const temporaryDifference =
+			actualDate.getTime() - temporaryInitialDate.getTime();
+		const temporaryDifferenceDays =
+			Math.ceil(temporaryDifference / (1000 * 3600 * 24)) - 1;
+		await dayModel.findOneAndUpdate(
+			{
+				id: 'expences',
+			},
+			{
+				counter: temporaryDifferenceDays,
+				month: temporaryActualMonth,
+				year: actualYear,
+				money: 5000,
+				moneyConstant: 5000,
+				food: 0,
+				fuel: 0,
+				medicines: 0,
+				bills: 0,
+				clothes: 0,
+				entertainment: 0,
+				chemicals: 0,
+				foodMonth: 0,
+				fuelMonth: 0,
+				alcoholMonth: 0,
+				medicinesMonth: 0,
+				billsMonth: 0,
+				clothesMonth: 0,
+				entertainmentMonth: 0,
+				chemicalsMonth: 0,
+			},
+		);
+		const data = await dayModel.findOne({ id: 'expences' });
+		res.json([data, dateData]);
+	} else {
+		//jeśli jest nowy dzień, zaktualizuj dane
+		await dayModel.findOneAndUpdate(
+			{
+				id: 'expences',
+			},
+			{
+				counter: differenceDays,
+				moneyConstant: money,
+				food: 0,
+				fuel: 0,
+				medicines: 0,
+				bills: 0,
+				clothes: 0,
+				entertainment: 0,
+				chemicals: 0,
+			},
+		);
+		const data = await dayModel.findOne({ id: 'expences' });
+		res.json([data, dateData]);
+	}
 });
 
-//   if(differenceDays > 27){
-//       if(actualDay >= 25){
-//         const temporaryInitialDate = new Date(`${actualMonth}/25/2021`);
-//         const temporaryDifference = actualDate.getTime() - temporaryInitialDate.getTime();
-//         const temporaryDifferenceDays = (Math.ceil(temporaryDifference / (1000 * 3600 * 24))) - 1;
-//         await dayModel.findOneAndUpdate(
-//           {
-//             id: 'expences'
-//           },{
-//             counter: temporaryDifferenceDays,
-//             month: actualMonth,
-//             year: actualYear,
-//             money: 2850,
-//             moneyConstant: 2850,
-//             food: 0,
-//             fuel: 0,
-//             alcohol: 0,
-//             medicines: 0,
-//             bills: 0,
-//             clothes: 0,
-//             entertainment: 0,
-//             foodMonth: 0,
-//             fuelMonth: 0,
-//             alcoholMonth: 0,
-//             medicinesMonth: 0,
-//             billsMonth: 0,
-//             clothesMonth: 0,
-//             entertainmentMonth: 0
-//           });
-//         const data = await dayModel.findOne({id: 'expences'})
-//         res.json([data, dateData]);
-//       }else if(actualDay < 25){
-//         const temporaryInitialDate = new Date(`${actualMonth - 1}/25/2021`);
-//       }
-//     }else {
-//       //jeśli jest nowy dzień, zaktualizuj dane
-//       await dayModel.findOneAndUpdate(
-//         {
-//           id: 'expences'
-//         },{
-//           counter: differenceDays,
-//           moneyConstant: money,
-//           food: 0,
-//           fuel: 0,
-//           alcohol: 0,
-//           medicines: 0,
-//           bills: 0,
-//           clothes: 0,
-//           entertainment: 0,
-//         });
-//     const data = await dayModel.findOne({id: 'expences'})
-//     res.json([data, dateData]);
-//   };
-// });
+app.post('/update', async (req, res) => {
+	const { sum, sumMonth, type, typeMonth, moneyMinusValue } = req.body;
+	await dayModel.findOneAndUpdate(
+		{
+			id: 'expences',
+		},
+		{
+			[type]: sum,
+			[typeMonth]: sumMonth,
+			money: moneyMinusValue,
+		},
+	);
+	const data = await dayModel.findOne({ id: 'expences' });
+	res.json([data, null]);
+});
 
-app.post('/update', async (req, res)=>{
-  const { sum, sumMonth, type, typeMonth, moneyMinusValue } = req.body;
-  await dayModel.findOneAndUpdate(
-    {
-      id: 'expences'
-    },{
-      [type]: sum,
-      [typeMonth]: sumMonth,
-      money: moneyMinusValue,
-    });
-  const data = await dayModel.findOne({id: 'expences'})
-  res.json([data, null]);
-})
-
-// new dayModel({
-// }).save();
+// new dayModel({}).save();
 
 const serverPort = process.env.PORT || 8080;
 app.listen(serverPort);
--- public/methods.js
+++ public/methods.js
@@ -1,112 +1,201 @@
-class Methods{
-  constructor(){
-    this.circlesWrapper = document.querySelector('.days-left__circles');
-    this.daysLeft = document.querySelector('.days-left__days');
-    this.graphs = [...document.querySelectorAll('.component__graph-inside')];
-    this.graphsMonth = [...document.querySelectorAll('.component__graph-inside-m')];
-    this.valueDayAll = document.querySelector('.value-day-all');
-    this.valueDaySum = document.querySelector('.value-day-sum');
-    this.valueMonthAll = document.querySelector('.value-month-all');
-    this.sumForDayElement = document.querySelector('.main-amount__number--for-day');
-    this.moneyLeftElement = document.querySelector('.main-amount__number--whole');
-    this.dateElement = document.querySelector('.expencess__day-header');
-    this.datesElement = document.querySelector('.expencess__all-header');
-  };
+class Methods {
+	constructor() {
+		this.circlesWrapper = document.querySelector('.days-left__circles');
+		this.daysLeft = document.querySelector('.days-left__days');
+		this.graphs = [...document.querySelectorAll('.component__graph-inside')];
+		this.graphsMonth = [
+			...document.querySelectorAll('.component__graph-inside-m'),
+		];
+		this.valueDayAll = document.querySelector('.value-day-all');
+		this.valueDaySum = document.querySelector('.value-day-sum');
+		this.valueMonthAll = document.querySelector('.value-month-all');
+		this.sumForDayElement = document.querySelector(
+			'.main-amount__number--for-day',
+		);
+		this.moneyLeftElement = document.querySelector(
+			'.main-amount__number--whole',
+		);
+		this.dateElement = document.querySelector('.expencess__day-header');
+		this.datesElement = document.querySelector('.expencess__all-header');
+	}
 
-  updateDayData(data){
-    const { counter, moneyConstant, year, month } = data[0];
-    const { actualDay, actualMonth, actualYear } = data[1];
-    let daysInLastMonth = new Date(year, month, 0).getDate();
-    let daysLeft = daysInLastMonth - counter;
+	updateDayData(data) {
+		const { counter, moneyConstant, year, month } = data[0];
+		const { actualDay, actualMonth, actualYear } = data[1];
+		let daysInLastMonth = new Date(year, month, 0).getDate();
+		let daysLeft = daysInLastMonth - counter;
 
-    this.fillCircles(counter, daysInLastMonth);
-    this.updateDaysLeft(daysLeft);
-    this.sumForDay = (moneyConstant/daysLeft).toFixed(1);
-    this.updateSumForDay();
-    this.updateDate( actualDay, actualMonth, actualYear );
-  };
+		this.fillCircles(counter, daysInLastMonth);
+		this.updateDaysLeft(daysLeft);
+		this.sumForDay = (moneyConstant / daysLeft).toFixed(1);
+		this.updateSumForDay();
+		this.updateDate(actualDay, actualMonth, actualYear);
+	}
 
-  updateDate( actualDay, actualMonth, actualYear ){
-    let monthsArrayD = ['stycznia','lutego','marca','kwietnia','maja','czerwca','lipca','sierpnia','września', 'października', 'listopada', 'grudnia'];
-    let monthsArrayM = ['styczeń','luty','marzec','kwiecień','maj','czerwiec','lipiec','sierpień','wrzesień', 'październik', 'listopad', 'grudzień'];
-    this.dateElement.textContent = `${actualDay} ${monthsArrayD[actualMonth - 1]} ${actualYear}`;
-    let makeUpper = [...monthsArrayM[actualMonth - 2]][0].toUpperCase() + monthsArrayM[actualMonth - 2].slice(1);
-    this.datesElement.textContent = `${makeUpper}/${monthsArrayM[actualMonth - 1]}`;
-  }
+	updateDate(actualDay, actualMonth, actualYear) {
+		let monthsArrayD = [
+			'stycznia',
+			'lutego',
+			'marca',
+			'kwietnia',
+			'maja',
+			'czerwca',
+			'lipca',
+			'sierpnia',
+			'września',
+			'października',
+			'listopada',
+			'grudnia',
+		];
+		let monthsArrayM = [
+			'styczeń',
+			'luty',
+			'marzec',
+			'kwiecień',
+			'maj',
+			'czerwiec',
+			'lipiec',
+			'sierpień',
+			'wrzesień',
+			'październik',
+			'listopad',
+			'grudzień',
+		];
+		this.dateElement.textContent = `${actualDay} ${
+			monthsArrayD[actualMonth - 1]
+		} ${actualYear}`;
+		let makeUpper =
+			[...monthsArrayM[actualMonth - 2]][0].toUpperCase() +
+			monthsArrayM[actualMonth - 2].slice(1);
+		this.datesElement.textContent = `${makeUpper}/${
+			monthsArrayM[actualMonth - 1]
+		}`;
+	}
 
-  updateCurrentData(data){
-    const {
-      money, food, fuel, alcohol, medicines, bills, clothes, entertainment, chemicals, 
-      foodMonth, fuelMonth, alcoholMonth, medicinesMonth, billsMonth, clothesMonth, entertainmentMonth, chemicalsMonth
-    } = data[0];
+	updateCurrentData(data) {
+		const {
+			money,
+			food,
+			fuel,
+			medicines,
+			bills,
+			clothes,
+			entertainment,
+			chemicals,
+			foodMonth,
+			fuelMonth,
+			medicinesMonth,
+			billsMonth,
+			clothesMonth,
+			entertainmentMonth,
+			chemicalsMonth,
+		} = data[0];
 
-    let expancesArray = [bills, fuel, food, alcohol, medicines, chemicals, clothes, entertainment];
-    let expancesMonthArray = [billsMonth, fuelMonth, foodMonth, alcoholMonth, medicinesMonth, chemicalsMonth, clothesMonth, entertainmentMonth];
-    let expancesNamesArray = ['bills', 'fuel', 'food', 'alcohol', 'medicines','chemicals', 'clothes', 'entertainment'];
-    let daySum = expancesArray.reduce((acc, item)=>{
-      return acc + item;
-    },0);
+		let expancesArray = [
+			bills,
+			fuel,
+			food,
+			medicines,
+			chemicals,
+			clothes,
+			entertainment,
+		];
+		let expancesMonthArray = [
+			billsMonth,
+			fuelMonth,
+			foodMonth,
+			medicinesMonth,
+			chemicalsMonth,
+			clothesMonth,
+			entertainmentMonth,
+		];
+		let expancesNamesArray = [
+			'bills',
+			'fuel',
+			'food',
+			'medicines',
+			'chemicals',
+			'clothes',
+			'entertainment',
+		];
+		let daySum = expancesArray.reduce((acc, item) => {
+			return acc + item;
+		}, 0);
 
-    let monthSum = expancesMonthArray.reduce((acc, item)=>{
-      return acc + item;
-    },0);
+		let monthSum = expancesMonthArray.reduce((acc, item) => {
+			return acc + item;
+		}, 0);
 
-    this.updateExpances(expancesNamesArray, expancesArray, expancesMonthArray, daySum, monthSum);
-    this.updateMoneyLeft(money);
-  };
+		this.updateExpances(
+			expancesNamesArray,
+			expancesArray,
+			expancesMonthArray,
+			daySum,
+			monthSum,
+		);
+		this.updateMoneyLeft(money);
+	}
 
-  updateMoneyLeft(money){
-    this.moneyLeftElement.textContent = money;
-  };
+	updateMoneyLeft(money) {
+		this.moneyLeftElement.textContent = money;
+	}
 
-  updateSumForDay(){
-    this.sumForDayElement.textContent = this.sumForDay + '0';
-  };
+	updateSumForDay() {
+		this.sumForDayElement.textContent = this.sumForDay + '0';
+	}
 
-  updateExpances(expancesNamesArray, expancesArray, expancesMonthArray, daySum, monthSum){
-    expancesNamesArray.forEach((expancesName, index) =>{
-       document.querySelector(`[data-dv="${expancesName}"]`).textContent = expancesArray[index];
-       let percents = ((expancesArray[index] / daySum) * 100).toFixed();
-       percents = isNaN(percents) ? 0 : percents;
-       this.graphs[index].style.width = `${percents}%`;
-    });
-    this.valueDayAll.textContent = daySum.toFixed(2);
+	updateExpances(
+		expancesNamesArray,
+		expancesArray,
+		expancesMonthArray,
+		daySum,
+		monthSum,
+	) {
+		expancesNamesArray.forEach((expancesName, index) => {
+			document.querySelector(`[data-dv="${expancesName}"]`).textContent =
+				expancesArray[index];
+			let percents = ((expancesArray[index] / daySum) * 100).toFixed();
+			percents = isNaN(percents) ? 0 : percents;
+			this.graphs[index].style.width = `${percents}%`;
+		});
+		this.valueDayAll.textContent = daySum.toFixed(2);
 
-    expancesNamesArray.forEach((expancesName, index) =>{
-      document.querySelector(`[data-month="${expancesName}"]`).textContent = expancesMonthArray[index];
-      let percents = ((expancesMonthArray[index] / monthSum) * 100).toFixed();
-      percents = isNaN(percents) ? 0 : percents;
-      this.graphsMonth[index].style.width = `${percents}%`;
-   });
-   this.valueMonthAll.textContent = monthSum.toFixed(2);
-    
-    let left = (this.sumForDay - daySum).toFixed(1);
-    let color = 'red';
-    if(left > 0){
-      left = `+${left}`;
-      color = 'green'
-    };
-    this.valueDaySum.textContent = left + '0';
-    this.valueDaySum.style.color = color;
-  };
+		expancesNamesArray.forEach((expancesName, index) => {
+			document.querySelector(`[data-month="${expancesName}"]`).textContent =
+				expancesMonthArray[index];
+			let percents = ((expancesMonthArray[index] / monthSum) * 100).toFixed();
+			percents = isNaN(percents) ? 0 : percents;
+			this.graphsMonth[index].style.width = `${percents}%`;
+		});
+		this.valueMonthAll.textContent = monthSum.toFixed(2);
 
-  fillCircles(counter, daysInLastMonth){
-    this.circlesWrapper.innerHTML = '';
-    for(let i = 0; i<daysInLastMonth; i++){
-      let div = document.createElement('div');
-      div.classList.add('days-left__circle');
-      if(i>=counter){
-        div.classList.add('days-left__circle--filled');
-      };
-      this.circlesWrapper.appendChild(div);
-    };
-  };
+		let left = (this.sumForDay - daySum).toFixed(1);
+		let color = 'red';
+		if (left > 0) {
+			left = `+${left}`;
+			color = 'green';
+		}
+		this.valueDaySum.textContent = left + '0';
+		this.valueDaySum.style.color = color;
+	}
 
-  updateDaysLeft(daysLeft){
-    let word;
-    daysLeft > 1 ? word = 'dni' : word = 'dzień';
-    this.daysLeft.textContent = `${daysLeft} ${word}`;
-  };
-};
+	fillCircles(counter, daysInLastMonth) {
+		this.circlesWrapper.innerHTML = '';
+		for (let i = 0; i < daysInLastMonth; i++) {
+			let div = document.createElement('div');
+			div.classList.add('days-left__circle');
+			if (i >= counter) {
+				div.classList.add('days-left__circle--filled');
+			}
+			this.circlesWrapper.appendChild(div);
+		}
+	}
+
+	updateDaysLeft(daysLeft) {
+		let word;
+		daysLeft > 1 ? (word = 'dni') : (word = 'dzień');
+		this.daysLeft.textContent = `${daysLeft} ${word}`;
+	}
+}
 
 export const methods = new Methods();
